9cdfe42fbc9a868830ba4b5f57f36db68217aec1
houzhizhen <houzhizhen@baidu.com>
2022-04-01 11:09:28 +0800

    improve hive-server-monitor.sh

--- bin/hive-server-monitor.sh
+++ bin/hive-server-monitor.sh
@@ -3,13 +3,13 @@
 # monitor the jmx and live ojbect in hive server
 cd $(dirname $0);pwd
 
-DELETE_LOGFILE=hive-server-jmx-`date -d "-1 day" +"%Y%m%d"`
+DELETE_LOGFILE=`date -d "-1 day" +"%Y%m%d"`
 rm -rf ${DELETE_LOGFILE}*
 HIVESERVER_JMX="http://localhost:10002/jmx"
-LOGFILE=hive-server-jmx-`date +"%Y%m%d-%H%M%S"`.log
+LOGFILE=`date +"%Y%m%d-%H%M%S"`-hive-server-jmx.log
 curl ${HIVESERVER_JMX} > ${LOGFILE}
 
-HISTO_LOGFILE=hive-server-histo-live-`date +"%Y%m%d-%H%M%S"`.log
+HISTO_LOGFILE=`date +"%Y%m%d-%H%M%S"`-hive-server-histo-live.log
 HIVESERVER_PID=`ps aux | grep HiveServer | grep -v grep | awk '{print $2}'`
 
 /opt/jdk1.8.0_211/bin/jmap -heap ${HIVESERVER_PID} > ${HISTO_LOGFILE}
