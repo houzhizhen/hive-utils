781ea04273610516c983e9ec85efa48b6226b160
houzhizhen <houzhizhen@baidu.com>
2022-05-16 19:50:36 +0800

    add ParallelStatementTest

---
+++ bin/parallel-jdbc-statement.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Parameters: "url" "execute times" "sql" "close connection"
+hadoop jar ./hive-util-0.1.0.jar com.baidu.hive.jdbc.ParallelStatementTest \
+ "jdbc:hive2://localhost:10000/default" \
+ 1000 \
+ "select * from t1 limit 1" \
+ true
---
+++ src/main/java/com/baidu/hive/jdbc/ParallelStatementTest.java
@@ -0,0 +1,91 @@
+package com.baidu.hive.jdbc;
+
+import com.baidu.hive.util.log.LogUtil;
+import org.apache.hive.jdbc.HiveDriver;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ParallelStatementTest {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, InterruptedException {
+        String hiveUrl = "jdbc:hive2://localhost:10000/default";
+        int times = 10;
+        String sql = "select * from t1 limit 1";
+        String userName = "hive";
+        boolean closeConnection = true;
+        if (args.length >0) {
+            hiveUrl = args[0];
+        }
+        if (args.length > 1) {
+            times = Integer.parseInt(args[1]);
+        }
+        if (args.length > 2) {
+            sql = args[2];
+        }
+        if (args.length > 3) {
+            closeConnection = Boolean.parseBoolean(args[3]);
+        }
+        if (args.length > 4) {
+            userName =args[4];
+        }
+        LogUtil.log("Parameters:");
+        LogUtil.log("hiveUrl = " + hiveUrl);
+        LogUtil.log("times = " + times);
+        LogUtil.log("sql = " + sql);
+        LogUtil.log("closeConnection = " + closeConnection);
+        LogUtil.log("userName = " + userName);
+        Class.forName(HiveDriver.class.getName());
+        AtomicBoolean error = new AtomicBoolean(false);
+        try {
+            for (int i = 0; i < times; i++) {
+                LogUtil.log("i = " + i);
+                Connection conn = java.sql.DriverManager.getConnection(hiveUrl, userName, "hive");
+                parallelExecute(conn, sql, 2, 3);
+                conn.close();
+            }
+        } catch(Throwable t) {
+            error.set(true);
+            t.printStackTrace();
+        } finally {
+
+            if (error.get()) {
+                LogUtil.log("Error Finished");
+            } else {
+                LogUtil.log("Success Finished");
+                // TimeUnit.SECONDS.sleep(3600);
+            }
+        }
+    }
+
+    public static void parallelExecute(Connection conn, String sql, int parallelism, int times) throws InterruptedException {
+        CountDownLatch countDown = new CountDownLatch(parallelism);
+        ExecutorService es = Executors.newFixedThreadPool(parallelism);
+        for (int i = 0; i < parallelism; i++) {
+            es.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("start countdown");
+                        countDown.countDown();
+                        System.out.println("end countdown");
+                        Statement st = conn.createStatement();
+                        for (int j = 0; j < times; j++) {
+                            st.executeQuery(sql);
+                        }
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        es.shutdownNow();
+        es.awaitTermination(60, TimeUnit.SECONDS);
+    }
+}
