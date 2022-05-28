8337f464a51575193de4af9c11d4848cb1ee9a66
houzhizhen <houzhizhen@baidu.com>
2022-05-28 16:18:43 +0800

    change class name of parallel-jdbc-statement

--- bin/parallel-jdbc-statement.sh
+++ bin/parallel-jdbc-statement.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Parameters: "url" "execute times" "sql" "close connection"
-hadoop jar ./hive-util-0.1.0.jar com.baidu.hive.jdbc.ParallelStatementTest \
+hive --service jar ./hive-util-0.1.0.jar com.baidu.hive.jdbc.ParallelStatementTest \
  "jdbc:hive2://localhost:10000/default" \
  1000 \
  "select * from t1 limit 1" \
