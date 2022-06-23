4d84f397ff6d6fceb7ee01b168641a45a3ed0d8e
houzhizhen <houzhizhen@baidu.com>
2022-06-23 11:29:17 +0800

    improve RemoveJdComment

--- src/main/java/com/baidu/hive/util/RemoveJdComment.java
+++ src/main/java/com/baidu/hive/util/RemoveJdComment.java
@@ -12,16 +12,22 @@ import java.io.IOException;
 public class RemoveJdComment {
 
     public static void main(String[] args) throws IOException {
-//        args = new String[]{"/home/houzhizhen/Downloads/ali-SemanticAnalyzer.java",
-//        "/home/houzhizhen/Downloads/c-ali-SemanticAnalyzer.java"};
+        args = new String[]{"/home/houzhizhen/dist/jdk8_301/URLClassPath.java",
+                "/home/houzhizhen/git/hive-utils/src/main/resources/URLClassPath.java"};
         BufferedReader reader = new BufferedReader(new FileReader(args[0]));
         BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
         String line = reader.readLine();
-        while (line != null && line.length() > 12) {
-            if (line.length() > 12 && line.startsWith("/*")) {
-                // Remove leading 12 characters.
-                writer.write(line.substring(12));
-                writer.write("\n");
+        while (line != null) {
+            if (line.startsWith("/*")) {
+                int endIndex = line.indexOf("*/");
+                if (endIndex == -1) {
+                    writer.write(line);
+                    writer.write("\n");
+                } else {
+                    String t = line.substring(endIndex + 2);
+                    writer.write(t);
+                    writer.write("\n");
+                }
             } else {
                 writer.write(line);
                 writer.write("\n");
