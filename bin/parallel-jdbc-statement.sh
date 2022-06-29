22c326c419619b5bfc8cb8c37ef06572e552d5bc
houzhizhen <houzhizhen@baidu.com>
2022-06-29 09:46:09 +0800

    modify parallel-jdbc-statement.sh

--- bin/parallel-jdbc-statement.sh
+++ bin/parallel-jdbc-statement.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Parameters: "url" "execute times" "sql" "close connection"
 hive --service jar ./hive-util-0.1.0.jar com.baidu.hive.jdbc.ParallelStatementTest \
- "jdbc:hive2://localhost:10000/default" \
- 1000 \
- "select * from t1 limit 1" \
+ "jdbc:hive2://master-79c33a5-1:10000/default;principal=hive/_HOST@BAIDU.COM" \
+ 10 \
+ "util.sm4encrypt('b001','123')" \
  true
