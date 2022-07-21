973b327bd8f216e06a55609f5c07daf9f3decef6
houzhizhen <houzhizhen@baidu.com>
2022-07-21 14:11:03 +0800

    add create function script

--- src/main/java/com/baidu/hive/func/Echo.java
+++ src/main/java/com/baidu/hive/func/Echo.java
@@ -3,7 +3,9 @@ package com.baidu.hive.func;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hive.ql.exec.UDF;
-
+/**
+ * create function echo as 'com.baidu.hive.func.Echo' using jar 'hdfs://localhost:9000/apps/hive/hive-util-0.1.0.jar';
+ */
 public class Echo extends UDF {
 
     private final static   Log LOG = LogFactory.getLog(Echo.class.getName());
--- src/main/java/com/baidu/hive/func/sleep/TimeSleep.java
+++ src/main/java/com/baidu/hive/func/sleep/TimeSleep.java
@@ -4,6 +4,9 @@ import org.apache.hadoop.hive.ql.exec.UDF;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * create function timesleep as 'com.baidu.hive.func.sleep.TimeSleep' using jar 'hdfs://localhost:9000/apps/hive/hive-util-0.1.0.jar';
+ */
 public class TimeSleep extends UDF {
 
     public long evaluate(long time) {
