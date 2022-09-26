a808a26a2d8a0fbe67378b7eed95b6108aca93aa
houzhizhen <houzhizhen@baidu.com>
2022-09-26 11:50:07 +0800

    add MultiThreadMetaStoreConnectTest PartitionGenerator

--- bin/multi-thread-metastore-connect-test.sh
+++ bin/multi-thread-metastore-connect-test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-hive --service jar ./hive-util-0.1.0.jar com.baidu.hive.metastore.MultiThreadMetaStoreConnectTest -hiveconf metastore.connection.count=3 file:///usr/local/hive/conf/hive-site.xml
+hive --service jar ./hive-util-0.1.0.jar com.baidu.hive.metastore.MultiThreadMetaStoreConnectTest \
+     -hiveconf metastore.connection.count=7200 file:///usr/local/hive/conf/hive-site.xml
