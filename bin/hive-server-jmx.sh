2fa17fab6ea6a871aa33b14a822ae5c32521172e
houzhizhen <houzhizhen@baidu.com>
2023-01-03 16:54:05 +0800

    update hive-server-jmx retain time from 1 day to 30 day

--- bin/hive-server-jmx.sh
+++ bin/hive-server-jmx.sh
@@ -3,7 +3,7 @@
 # monitor the jmx and live ojbect in hive server
 cd $(dirname $0);pwd
 
-DELETE_LOGFILE=`date -d "-1 day" +"%Y%m%d"`
+DELETE_LOGFILE=`date -d "-30 day" +"%Y%m%d"`
 rm -rf ${DELETE_LOGFILE}*
 HIVESERVER_JMX="http://localhost:10002/jmx"
 LOGFILE=`date +"%Y%m%d-%H%M%S"`-hive-server-jmx.log
